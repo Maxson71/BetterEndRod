d1a930e9a41597bd93830172fd85a80182ea9329
Maxson71 <sherstukmax2005@gmail.com>
2026-06-07 20:46:37 +0300

    Support Minecraft 1.21.8 and 1.21.11 builds

--- src/main/java/com/maxson/betterendrod/BetterEndRodClient.java
+++ src/main/java/com/maxson/betterendrod/BetterEndRodClient.java
@@ -3,24 +3,13 @@ package com.maxson.betterendrod;
 import com.maxson.betterendrod.block.BlocksMod;
 import com.maxson.betterendrod.particle.ParticlesMod;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.minecraft.client.particle.EndRodParticle;
-import net.minecraft.client.render.RenderLayer;
 
 public class BetterEndRodClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
-        BlockRenderLayerMap.INSTANCE.putBlock(BlocksMod.REDSTONE_ENDROD, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(BlocksMod.SAND_ENDROD, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(BlocksMod.EMERALD_ENDROD, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(BlocksMod.AMETHYST_ENDROD, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(BlocksMod.COPPER_ENDROD, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(BlocksMod.RUSTY_ENDROD, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(BlocksMod.HONEY_ENDROD, RenderLayer.getCutout());
-
         ParticleFactoryRegistry.getInstance().register(ParticlesMod.REDSTONE_ENDROD, EndRodParticle.Factory::new);
         ParticleFactoryRegistry.getInstance().register(ParticlesMod.SAND_ENDROD, EndRodParticle.Factory::new);
         ParticleFactoryRegistry.getInstance().register(ParticlesMod.HONEY_ENDROD, EndRodParticle.Factory::new);
--- src/main/java/com/maxson/betterendrod/block/EndRodBlock.java
+++ src/main/java/com/maxson/betterendrod/block/EndRodBlock.java
@@ -16,7 +16,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+import net.minecraft.world.tick.ScheduledTickView;
 
 public class EndRodBlock extends RodBlock implements Waterloggable {
     private final ParticleEffect particleType;
@@ -53,7 +54,7 @@ public class EndRodBlock extends RodBlock implements Waterloggable {
         double f = (double)pos.getZ() + 0.55 - (double)(random.nextFloat() * 0.1F);
         double g = (double)(0.4F - (random.nextFloat() + random.nextFloat()) * 0.4F);
         if (random.nextInt(5) == 0) {
-            world.addParticle(this.particleType, d + (double)direction.getOffsetX() * g, e + (double)direction.getOffsetY() * g, f + (double)direction.getOffsetZ() * g, random.nextGaussian() * 0.005, random.nextGaussian() * 0.005, random.nextGaussian() * 0.005);
+            world.addParticleClient(this.particleType, d + (double)direction.getOffsetX() * g, e + (double)direction.getOffsetY() * g, f + (double)direction.getOffsetZ() * g, random.nextGaussian() * 0.005, random.nextGaussian() * 0.005, random.nextGaussian() * 0.005);
         }
     }
 
@@ -63,11 +64,11 @@ public class EndRodBlock extends RodBlock implements Waterloggable {
     }
 
     @Override
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+    protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
         if (state.get(Properties.WATERLOGGED)) {
-            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+            tickView.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
         }
-        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+        return super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
     }
 
     @Override
