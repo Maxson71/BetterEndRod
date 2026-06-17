d947c9189afdb45f86b600e0cc10d57f55290ebd
Maxson71 <sherstukmax2005@gmail.com>
2026-06-17 21:53:35 +0300

    Fix block registration for Minecraft 1.21.11

--- src/main/java/com/maxson/betterendrod/block/BlocksMod.java
+++ src/main/java/com/maxson/betterendrod/block/BlocksMod.java
@@ -7,35 +7,35 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.particle.ParticleEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
 
 public class BlocksMod {
-    public static final Block SAND_ENDROD = registerBlock("sand_endrod",
-            new EndRodBlock(AbstractBlock.Settings.copy(Blocks.END_ROD), ParticlesMod.SAND_ENDROD));
-    public static final Block REDSTONE_ENDROD = registerBlock("redstone_endrod",
-            new EndRodBlock(AbstractBlock.Settings.copy(Blocks.END_ROD), ParticlesMod.REDSTONE_ENDROD));
-    public static final Block HONEY_ENDROD = registerBlock("honey_endrod",
-            new EndRodBlock(AbstractBlock.Settings.copy(Blocks.END_ROD), ParticlesMod.HONEY_ENDROD));
-    public static final Block COPPER_ENDROD = registerBlock("copper_endrod",
-            new EndRodBlock(AbstractBlock.Settings.copy(Blocks.END_ROD), ParticlesMod.COPPER_ENDROD));
-    public static final Block AMETHYST_ENDROD = registerBlock("amethyst_endrod",
-            new EndRodBlock(AbstractBlock.Settings.copy(Blocks.END_ROD), ParticlesMod.AMETHYST_ENDROD));
-    public static final Block RUSTY_ENDROD = registerBlock("rusty_endrod",
-            new EndRodBlock(AbstractBlock.Settings.copy(Blocks.END_ROD), ParticlesMod.RUSTY_ENDROD));
-    public static final Block EMERALD_ENDROD = registerBlock("emerald_endrod",
-            new EndRodBlock(AbstractBlock.Settings.copy(Blocks.END_ROD), ParticlesMod.EMERALD_ENDROD));
-    public static final Block SCULK_ENDROD = registerBlock("sculk_endrod",
-            new EndRodBlock(AbstractBlock.Settings.copy(Blocks.END_ROD), ParticlesMod.SCULK_ENDROD));
+    public static final Block SAND_ENDROD = registerEndRodBlock("sand_endrod", ParticlesMod.SAND_ENDROD);
+    public static final Block REDSTONE_ENDROD = registerEndRodBlock("redstone_endrod", ParticlesMod.REDSTONE_ENDROD);
+    public static final Block HONEY_ENDROD = registerEndRodBlock("honey_endrod", ParticlesMod.HONEY_ENDROD);
+    public static final Block COPPER_ENDROD = registerEndRodBlock("copper_endrod", ParticlesMod.COPPER_ENDROD);
+    public static final Block AMETHYST_ENDROD = registerEndRodBlock("amethyst_endrod", ParticlesMod.AMETHYST_ENDROD);
+    public static final Block RUSTY_ENDROD = registerEndRodBlock("rusty_endrod", ParticlesMod.RUSTY_ENDROD);
+    public static final Block EMERALD_ENDROD = registerEndRodBlock("emerald_endrod", ParticlesMod.EMERALD_ENDROD);
+    public static final Block SCULK_ENDROD = registerEndRodBlock("sculk_endrod", ParticlesMod.SCULK_ENDROD);
 
-    private static Block registerBlock(String id, Block block){
+    private static Block registerEndRodBlock(String path, ParticleEffect particleType) {
+        Identifier id = BetterEndRod.id(path);
+        RegistryKey<Block> blockKey = RegistryKey.of(RegistryKeys.BLOCK, id);
+        Block block = new EndRodBlock(AbstractBlock.Settings.copy(Blocks.END_ROD).registryKey(blockKey), particleType);
         registerBlockItem(id, block);
-        return Registry.register(Registries.BLOCK, BetterEndRod.id(id), block);
+        return Registry.register(Registries.BLOCK, blockKey, block);
     }
 
-    private static Item registerBlockItem(String id, Block block){
-        return Registry.register(Registries.ITEM, BetterEndRod.id(id),
-                new BlockItem(block, new Item.Settings()));
+    private static Item registerBlockItem(Identifier id, Block block){
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, id);
+        return Registry.register(Registries.ITEM, itemKey,
+                new BlockItem(block, new Item.Settings().registryKey(itemKey).useBlockPrefixedTranslationKey()));
     }
 
     public static void register(){
